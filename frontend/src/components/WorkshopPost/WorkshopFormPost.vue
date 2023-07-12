38fcd42737b903a0af9d6399dddff2ca6e21b3ec
PHEAROM.CHEA <PHEAROM.CHEA@PNC.LAN>
2023-07-12 13:30:08 +0700

    [VG5-51]-create validation for form input

--- frontend/src/components/WorkshopPost/WorkshopFormPost.vue
+++ frontend/src/components/WorkshopPost/WorkshopFormPost.vue
@@ -3,50 +3,80 @@
     <v-container class="container">
       <h1>ADD WORKSHOP</h1>
       <div class="namefilled input">
-        <v-col cols="12">
+        <v-col cols="11">
           <v-text-field
-            :rules="nameRules"
-            :counter="10"
-            label="Title"
+            :rules="titleRules"
+            v-model="title"
+            label="Put your title for your workshop"
           ></v-text-field>
         </v-col>
       </div>
       <div class="namefilled input">
-        <v-col cols="12" md="6">
-          <v-text-field type="date" label="Date start"></v-text-field>
+        <v-col cols="12" md="5">
+          <v-text-field
+            type="date"
+            :rules="dateRules"
+            v-model="date"
+            label="Date start"
+          ></v-text-field>
         </v-col>
 
         <v-col cols="12" md="6">
-          <v-text-field type="time" label="Time Start"></v-text-field>
+          <v-text-field
+            type="time"
+            :roles="timeRoles"
+            v-model="time"
+            label="Time Start"
+            required
+          ></v-text-field>
         </v-col>
       </div>
       <div class="namefilled input">
-        <v-col cols="12" md="6">
-          <v-text-field label="Organizer"></v-text-field>
+        <v-col cols="12" md="5">
+          <v-text-field 
+          :roles:="ordanizerRoles"
+          v-model="organizer" 
+          label="Organizer"></v-text-field>
         </v-col>
 
-        <v-col cols="12" md="6" :rules="emailRules">
-          <v-text-field label="Contact"></v-text-field>
+        <v-col cols="12" md="6">
+          <v-text-field
+            :rules="contactRoles"
+            v-model="contact"
+            label="Contact with email"
+            required
+          ></v-text-field>
         </v-col>
       </div>
       <div class="namefilled input">
-        <v-col cols="12" md="6" :rules="emailRules">
-          <v-text-field label="Location" counter></v-text-field>
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="location"
+            label="Location"
+            counter
+          ></v-text-field>
         </v-col>
-        <v-col cols="12" md="6">
-          <v-file-input accept="image/*" label="Put your image"></v-file-input>
+        <v-col cols="12" md="7">
+          <v-file-input
+            accept="image/*"
+            v-model="image"
+            label="Put your image"
+          ></v-file-input>
         </v-col>
       </div>
       <div class="inputFilled">
-        <v-col cols="12">
+        <v-col cols="11">
           <v-textarea
             name="input-10-1"
+            :roles="rules"
+            v-model="description"
             label="Description"
+            required
             counter
           ></v-textarea>
         </v-col>
       </div>
-      <v-col cols="12" class="d-flex justify-space-between align-center">
+      <v-col cols="12" class="d-flex justify-space-around align-center">
         <v-btn class="ma-2" color="red-darken-1" size="x-large">
           <v-icon start icon="mdi-arrow-left"></v-icon>
           CANCEL
@@ -64,37 +94,42 @@ import "@mdi/font/css/materialdesignicons.css";
 export default {
   data: () => ({
     valid: true,
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-    verify: "",
+    title: "",
+    date: "",
+    time: "",
+    organizer: "",
+    contact: "",
+    location: "",
+    image: "",
+    description: "",
     users: [],
-    nameRules: [
+    titleRules: [
       (value) => {
         if (value) return true;
-
-        return "Name is required.";
+        return "Title is requird.";
       },
+    ],
+    dateRules: [
       (value) => {
-        if (value?.length <= 10) return true;
-
-        return "Name must be less than 10 characters.";
+        if (value) return true;
+        return "Date is requird.";
       },
     ],
-    loginEmailRules: [
-      (v) => !!v || "Required",
-      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    timeRoles: [
+      (value) => {
+        if (value) return true;
+        return "Date is requird.";
+      },
     ],
-    emailRules: [
-      (v) => !!v || "Required",
+    contactRoles: [
+      (v) => !!v || "Email is Required",
       (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
     ],
 
     show1: false,
     rules: {
       required: (value) => !!value || "Required.",
-      min: (v) => (v && v.length >= 8) || "Min 8 characters",
+      min: (v) => (v && v.length >= 10) || "Min 10 characters",
     },
   }),
   computed: {
@@ -103,14 +138,6 @@ export default {
     },
   },
   methods: {
-    goToDetailInformation() {
-      this.$router.push("/userinformation");
-    },
-    validate() {
-      if (this.$refs.loginForm.validate()) {
-        // submit form to server/API here...
-      }
-    },
     reset() {
       this.$refs.form.reset();
     },
@@ -133,7 +160,7 @@ export default {
   align-items: center;
 }
 .form {
-  width: 70%;
+  width: 60%;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -141,7 +168,7 @@ export default {
 }
 
 .container {
-  margin: 20px 0;
+  margin: 20px 0px;
   border-radius: 20px;
   box-shadow: 0 0 30px 1px #0077b6;
   display: flex;
