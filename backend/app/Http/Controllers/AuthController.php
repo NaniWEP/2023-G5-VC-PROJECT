8e2ac44a5ae64bef6e71cd64f9858f14a6558e66
PHEAROM.CHEA <PHEAROM.CHEA@PNC.LAN>
2023-07-13 14:32:11 +0700

    [VG5-00] Merge branch main to university-page

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -4,11 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AuthLoginRequest;
 use App\Http\Requests\AuthRegisterRequest;
-<<<<<<< HEAD
-=======
 use App\Http\Resources\GetUserResource;
 use App\Models\SchoolManager;
->>>>>>> 89264386a5213f321a0e9672e002216d38252cc0
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -22,10 +19,10 @@ class AuthController extends Controller
     public function register(AuthRegisterRequest $request)
     {
         $user = User::create([
-           'first_name' => $request -> first_name, 
+           'first_name' => $request -> first_name,
            'last_name' => $request -> last_name,
            'email' => $request -> email,
-           'password' => Hash::make($request -> password), 
+           'password' => Hash::make($request -> password),
            'date_of_birth' => $request -> date_of_birth,
            'province' => $request -> province,
            'role_id' => $request -> role_id,
@@ -43,7 +40,7 @@ class AuthController extends Controller
 
     public function login(AuthLoginRequest $request)
     {
-        
+
         $userEmail = User::where('email', $request->email)->first();
         $credential = $request->only('email', 'password');
 
@@ -68,7 +65,7 @@ class AuthController extends Controller
             $user = Auth::user();
             $user = new GetUserResource($user);
             $token = $user->createToken('API TOKEN', ['select', 'create', 'update', 'delete'])->plainTextToken;
-            
+
             return response()->json([
                 'success' => true,
                 'user' => $user,
