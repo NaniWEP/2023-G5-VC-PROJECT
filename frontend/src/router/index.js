2cfb4c1f87e53a1a2b5a6fc160d982ed06dee1dc
SOCHEAT.LUN <SOCHEAT.LUN@pnc.lan>
2023-07-19 13:27:35 +0700

    [VG5-290]Fix bug

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,27 +3,18 @@ import HomePageView from '../views/Page/HomeView.vue'
 import AboutPageView from '../views/Page/AboutView.vue'
 import UniversityPageView from '../views/Page/universities/UniversityView.vue'
 import WorkshopView from '../views/Page/WorkshopView.vue'
-
 import LoginPageView from '../views/Page/LoginView.vue'
 import RegisterPageView from '../views/Page/registers/RegisterView.vue'
-
 import UniversityDetalView from '../views/Page/universities/UniversityDetailView.vue'
-
 import StudentDetail from "../views/Page/dashboards/students/StudentDetailView.vue"
 import StudentFavoritePage from "../views/Page/dashboards/students/StudentFavoriteView.vue"
-
-import UserInformationView from '../views/Page/registers/UserInformationView.vue'
-
 import SearchView from '../views/SearchView.vue'
-
 import UserView from "../components/dashboards/managers/UserView.vue"
-
 import ApplyView from "../views/Page/dashboards/managers/applys/ApplyingView.vue"
 import ChartView from "../views/Page/dashboards/managers/charts/ChartView.vue"
 import PostView from "../views/Page/dashboards/managers/posts/PostView.vue"
 import MajorView from "../views/Page/dashboards/managers/majors/MajorView.vue"
 import ExpirationView from "../views/Page/dashboards/managers/expirations/ExpirationView.vue"
-// import DashboardView from "../views/Dashboard/DashboardView.vue"
 
 
 const routes = [
@@ -80,12 +71,6 @@ const routes = [
     path: "/favoritePage",
     name: "favoritePage",
     component: StudentFavoritePage,
-  },
-  {
-    path: '/userinformation',
-    name: 'userinformation',
-    component: UserInformationView,
-    props: true
   },
   
   {
