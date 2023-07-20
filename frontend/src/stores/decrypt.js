5c05fd09a7af7c59fdb2b28d0a15e27fb20f4881
Nan-151 <betchamnan151@gmail.com>
2023-07-20 14:51:38 +0700

    [VG5-164] Change button register to profile icon

--- frontend/src/stores/decrypt.js
+++
@@ -1,9 +0,0 @@
-import CryptoJS from 'crypto-js';
-// import CryptoJS from 'crypto-js';
-
-function decryptData(data, secretKey){
-    if(data!==undefined){
-        return CryptoJS.AES.decrypt(data, secretKey).toString(CryptoJS.enc.Utf8);
-    }
-}
-export default decryptData;
